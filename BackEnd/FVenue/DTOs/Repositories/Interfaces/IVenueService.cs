5dbc21d7d81e1ef0e481cb45d739d9941ef482a1
Ford <huynhduy20042002@gmail.com>
2024-03-21 10:33:23 +0700

    Format FV-76

--- BackEnd/FVenue/DTOs/Repositories/Interfaces/IVenueService.cs
+++ BackEnd/FVenue/DTOs/Repositories/Interfaces/IVenueService.cs
@@ -6,6 +6,7 @@ namespace DTOs.Repositories.Interfaces
     {
         List<Venue> GetVenues();
         Venue GetVenue(int id);
+        List<Venue> GetVenuesBySubCategory(int subCategoryId);
         KeyValuePair<bool, string> InsertVenue(Venue venue);
         KeyValuePair<bool, string> UpdateVenue(Venue venue);
         KeyValuePair<bool, string> DeleteVenue(int id);
